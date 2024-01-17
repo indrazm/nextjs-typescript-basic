4911679058c107946f0b9e284ff34166e20b98cd
Indra Zulfi Mushoddaq <me@indrazm.com>
2024-01-18 01:08:16 +0700

    chore: adding and init setup nextauth

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,19 @@
+'use client'
+
+import { signIn } from 'next-auth/react'
+
 export default function Page() {
-  return <h1>Hey There!</h1>
+  return (
+    <main>
+      <h1>Hey There!</h1>
+      <button
+        className="bg-indigo-500 text-white px-4 py-2 rounded-xl"
+        onClick={async () => {
+          await signIn('github')
+        }}
+      >
+        Sign In with GitHub
+      </button>
+    </main>
+  )
 }
