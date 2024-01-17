6ca722ec6f2f4341cf47e20d1ca5234faa37af38
Indra Zulfi Mushoddaq <me@indrazm.com>
2024-01-18 00:39:55 +0700

    chore: initiating husky

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,12 +1,12 @@
-import { Inter as FontSans } from "next/font/google";
-import "@/styles/globals.css";
+import { Inter as FontSans } from 'next/font/google'
+import '@/styles/globals.css'
 
-const fontSans = FontSans({ subsets: ["latin"] });
+const fontSans = FontSans({ subsets: ['latin'] })
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html>
       <body className={fontSans.className}>{children}</body>
     </html>
-  );
+  )
 }
