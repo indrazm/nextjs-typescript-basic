409ac474d21353681aa27a1e2767faafbce81c74
Indra Zulfi Mushoddaq <me@indrazm.com>
2024-01-18 00:57:37 +0700

    chore: add prisma and init

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,3 @@
 export default function Page() {
-  const user = {
-    name: 'John Doe',
-    email: 'john@domain.com'
-  }
-
-  const { name } = user
-  console.log(name)
-
   return <h1>Hey There!</h1>
 }
